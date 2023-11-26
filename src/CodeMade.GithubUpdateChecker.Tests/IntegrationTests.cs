053f618b2f6705204384353f3684d218794e9d71
Loris Bognanni <loris@codemade.co.uk>
2023-11-26 18:17:55 +0000

    :test_tube: Add manual integration test

---
+++ src/CodeMade.GithubUpdateChecker.Tests/IntegrationTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeMade.GithubUpdateChecker.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        //[Ignore("Local development only")]
+        [Test]
+        public async Task Test_WithRealData()
+        {
+            var checker = VersionChecker.Create("LBognanni", "ImageViewer", new Version(0,0,0), "Image Viewer");
+            await checker.NotifyIfNewVersion();
+        }
+    }
+}
